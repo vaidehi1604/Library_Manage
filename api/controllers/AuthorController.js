dce9a53df43069389298c3d63effa77256b46509
vaidehi1604 <vaidehip@zignuts.com>
2023-03-22 14:49:13 +0530

    CRUD:edit,delete,update,pagination

--- api/controllers/AuthorController.js
+++ api/controllers/AuthorController.js
@@ -15,8 +15,7 @@ module.exports = {
       const authors = await Author.create({
         author,
         mobileNo,
-      });
-      console.log(authors);
+      }).fetch();
       return res.status(201).json({
         message: sails.__("addDatta", lang),
         authors: authors,
@@ -37,7 +36,7 @@ module.exports = {
       const authors = await Author.find(id);
       //checke author find or not
       if (authors) {
-     //update author
+        //update author
         const updateAuthor = await Author.updateOne({ id: id }).set({
           author: author,
           mobileNo: mobileNo,
@@ -45,7 +44,7 @@ module.exports = {
         console.log(updateAuthor);
         return res.status(200).json({
           message: sails.__("dataUpdated", lang),
-          updateAuthor:updateAuthor
+          updateAuthor: updateAuthor,
         });
       } else {
         return res.status(500).json({
@@ -54,8 +53,53 @@ module.exports = {
       }
     } catch (error) {
       return res.status(500).json({
+        error:error,
         message: sails.__("notUpdated", lang),
       });
     }
   },
+  //delete author data
+  deleteAuthor: async (req, res) => {
+    const lang = req.getLocale();
+    try {
+      const { id } = req.params;
+      const author = await Author.find({ id });
+      if (author) {
+        const deleteAuthor = await Author.destroyOne({ id: id });
+        console.log(deleteAuthor);
+        return res.status(200).json({
+          message: sails.__("deleteData", lang),
+        });
+      } else {
+        //data not deleted or category not found
+        return res.status(404).json({
+          message: sails.__("notDeleted", lang),
+        });
+      }
+    } catch (error) {
+      return res.status(200).json({
+        error:error,
+        message: sails.__("notDeleted", lang),
+      });
+    }
+  },
+  getAuthor:async(req,res)=>{
+    const lang=req.getLocale();
+    try {
+        const limit = req.query.limit || 2;
+        const skip = req.query.skip || 0;
+        //get category with pagination
+        const getAuthor = await Author.find({ limit: limit, skip: skip });
+  
+        return res.status(200).json({
+          message: sails.__("getData", lang),
+          getAuthor: getAuthor,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            error:error,
+            message: sails.__("notGet", lang),
+          });
+    }
+  }
 };
